90a169a6452791ebc82e8bb052c365918ccc095e
kobakakzu0429 <kobakazu10@gmail.com>
2020-05-05 00:33:42 +0900

    design basic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello, world");
+import "normalize.css";
+import "./style.css";
+
+import oneup from "./assets/1up.ogg";
+
+const addPlayListener = (elementId: string, src: string) => {
+  const audio = new Audio();
+  audio.preload = "none";
+  audio.src = src;
+
+  const element = document.getElementById(elementId);
+  if (!element) return;
+  element.addEventListener("click", () => {
+    audio.play();
+  });
+};
+
+window.addEventListener("load", () => {
+  addPlayListener("1up", oneup);
+});
