e207e7d38331bd43b62061434ce7e6359d4ca5fb
kobakakzu0429 <kobakazu10@gmail.com>
2020-05-05 21:33:27 +0900

    add ogp setteing

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
       scriptLoading: "defer"
     }),
     new CopyPlugin([
-      { from: path.resolve(__dirname, "fonts"), to: path.resolve(__dirname, "dist") }
+      { from: path.resolve(__dirname, "public"), to: path.resolve(__dirname, "dist") }
     ]),
     new MiniCssExtractPlugin({
       filename: '[name].css',
